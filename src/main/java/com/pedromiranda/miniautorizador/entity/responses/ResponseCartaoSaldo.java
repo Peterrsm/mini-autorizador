e677ef7206f4287e21faecbfee4966f2af993375
peterrsm <pedrorsmiranda@gmail.com>
2023-11-21 17:43:47 -0300

    + ResponseCartaoSaldo

---
+++ src/main/java/com/pedromiranda/miniautorizador/entity/responses/ResponseCartaoSaldo.java
@@ -0,0 +1,18 @@
+package com.pedromiranda.miniautorizador.entity.dto;
+
+import com.pedromiranda.miniautorizador.entity.Cartao;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+public class ResponseCartaoSaldo implements Serializable {
+    private BigDecimal saldo;
+
+    public static ResponseCartaoSaldo cartaoToResponseCartaoSaldo(Cartao cartao) {
+        ResponseCartaoSaldo response = new ResponseCartaoSaldo();
+        response.saldo = cartao.getSaldo();
+        return response;
+    }
+}
