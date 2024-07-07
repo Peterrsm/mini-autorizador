c397e9f18553572a60f80185818d81128479700f
peterrsm <pedrorsmiranda@gmail.com>
2024-07-07 17:59:51 -0300

    + CartaoMapper refactor with the implementation of ICartaoMapper interface

--- src/main/java/com/pedromiranda/miniautorizador/entity/mapper/CartaoMapper.java
+++ src/main/java/com/pedromiranda/miniautorizador/entity/mapper/CartaoMapper.java
@@ -2,10 +2,12 @@ package com.pedromiranda.miniautorizador.entity.mapper;
 
 import com.pedromiranda.miniautorizador.entity.Cartao;
 import com.pedromiranda.miniautorizador.entity.dto.CartaoDTO;
+import com.pedromiranda.miniautorizador.entity.mapper.interfaces.ICartaoMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CartaoMapper {
+public class CartaoMapper implements ICartaoMapper {
+    @Override
     public Cartao toCartao(CartaoDTO cartao_dto) {
         return Cartao.CartaoBuilder.builder()
                 .numeroCartao(cartao_dto.getNumeroCartao())
